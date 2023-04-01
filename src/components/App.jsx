08fbd2acd5819ccb82324e95902f0b75b965d0ce
VadimKorobec <_s_e_t__@ukr.net>
2023-04-01 18:09:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
+import { List } from './List/List';
 import listSvg from '../assets/img/Vector.svg';
 
 export const App = () => {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-        <ul className="todo__list">
-          <li className="active">
-            <i>
-              <img src={listSvg} alt="list icon" />
-            </i>
-            <span>All tasks</span>
-          </li>
-        </ul>
+        <List
+          items={[
+            {
+              icon: <img src={listSvg} alt="list icon" />,
+              name: 'All tasks',
+            },
+          ]}
+        />
+        <List
+          items={[
+            {
+              color: '#42B883',
+              name: 'Shopping',
+            },
+            {
+              color: '#64C4ED',
+              name: 'Frontend',
+            },
+            {
+              color: '#FFBBCC',
+              name: 'Movies and serials',
+            },
+          ]}
+        />
       </div>
       <div className="todo__tasks"></div>
     </div>
