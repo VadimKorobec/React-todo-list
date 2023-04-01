c18065722c10c27457493ed356b05b5a0d10f2e9
VadimKorobec <_s_e_t__@ukr.net>
2023-04-01 16:44:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
+import listSvg from '../assets/img/Vector.svg';
+
 export const App = () => {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-        <button>Hello</button>
+        <ul className="todo__list">
+          <li className="active">
+            <i>
+              <img src={listSvg} alt="list icon" />
+            </i>
+            <span>All tasks</span>
+          </li>
+        </ul>
       </div>
       <div className="todo__tasks"></div>
     </div>
