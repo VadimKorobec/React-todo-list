d97877c0f0e66c46d7728f9bee2a0979f088b33e
VadimKorobec <_s_e_t__@ukr.net>
2023-04-01 15:25:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 export const App = () => {
   return (
     <div className="todo">
-      <div className="todo__sidebar"></div>
+      <div className="todo__sidebar">
+        <button>Hello</button>
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 };
