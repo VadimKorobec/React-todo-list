7806ec0d8dbd409190386f8e36fb85aa913762af
VadimKorobec <_s_e_t__@ukr.net>
2023-04-07 18:27:24 +0300

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import classNames from 'classnames';
-import '../List/List.scss';
 
-export const List = ({ items, isRemovable }) => {
+import '../List/List.scss';
+import { Badge } from 'components/Badge/Badge';
+
+export const List = ({ items, isRemovable, onClick }) => {
   return (
-    <>
-      <ul className="list">
+    <div>
+      <ul onClick={onClick} className="list">
         {items.map((item, index) => (
           <li
             key={index}
             className={classNames(item.className, { active: item.active })}
           >
-            <i>
-              {item.icon ? (
-                item.icon
-              ) : (
-                <i className={`badge badge--${item.color}`}></i>
-              )}
-            </i>
+            <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
             <span>{item.name}</span>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
