323e6ffc8af9b74cc071dfb727dbdba67ebb29bf
VadimKorobec <_s_e_t__@ukr.net>
2023-04-07 18:27:36 +0300

    Update AddButtonList.jsx

--- src/components/AddButtonList/AddButtonList.jsx
+++ src/components/AddButtonList/AddButtonList.jsx
@@ -1,10 +1,17 @@
 import { List } from '../List/List';
 import plusSvg from '../../assets/img/Group.svg';
+import { useState } from 'react';
 
-export const AddButtonList = () => {
+import '../AddButtonList/AddButtonList.scss';
+import { Badge } from 'components/Badge/Badge';
+
+export const AddButtonList = ({ colors }) => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+
   return (
-    <div>
+    <div className="add-list">
       <List
+        onClick={() => setVisiblePopup(!visiblePopup)}
         items={[
           {
             className: 'list__add-button',
@@ -13,7 +20,20 @@ export const AddButtonList = () => {
           },
         ]}
       />
-      <div className="add-list-form"></div>
+      {visiblePopup && (
+        <div className="add-list__popup">
+          <input className="field" type="text" placeholder="Folder name" />
+          <div className="add-list__popup-colors">
+            <ul>
+              <li>
+                <Badge color="green" />
+              </li>
+              <li></li>
+            </ul>
+          </div>
+          <button className="button">Add Folder</button>
+        </div>
+      )}
     </div>
   );
 };
