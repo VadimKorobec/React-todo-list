1da634a8ccc42c3418c4d6efe0ddfa006ffd63c0
VadimKorobec <_s_e_t__@ukr.net>
2023-04-07 15:35:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { List } from './List/List';
 import listSvg from '../assets/img/Vector.svg';
+import plusSvg from '../assets/img/Group.svg';
 
 export const App = () => {
   return (
@@ -27,8 +28,20 @@ export const App = () => {
             {
               color: 'pink',
               name: 'Movies and serials',
+              active: true,
             },
           ]}
+          isRemovable
+        />
+        <List
+          items={[
+            {
+              className: 'list__add-button',
+              icon: <img src={plusSvg} alt="add icon" />,
+              name: 'Add List ',
+            },
+          ]}
+          isRemovable
         />
       </div>
       <div className="todo__tasks"></div>
