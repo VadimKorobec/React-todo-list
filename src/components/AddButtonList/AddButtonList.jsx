13d708e00d2f60cf6e4426cb0b6d80c21c0f55e9
VadimKorobec <_s_e_t__@ukr.net>
2023-04-07 16:27:34 +0300

    Create AddButtonList.jsx

---
+++ src/components/AddButtonList/AddButtonList.jsx
@@ -0,0 +1,19 @@
+import { List } from '../List/List';
+import plusSvg from '../../assets/img/Group.svg';
+
+export const AddButtonList = () => {
+  return (
+    <div>
+      <List
+        items={[
+          {
+            className: 'list__add-button',
+            icon: <img src={plusSvg} alt="add icon" />,
+            name: 'Add List ',
+          },
+        ]}
+        isRemovable
+      />
+    </div>
+  );
+};
