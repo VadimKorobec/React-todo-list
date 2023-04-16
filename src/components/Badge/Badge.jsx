475caffee457e87a488c571149c58b1029477880
VadimKorobec <_s_e_t__@ukr.net>
2023-04-16 16:25:29 +0300

    Update Badge.jsx

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,5 +1,9 @@
 import './Badge.scss';
+import classNames from 'classnames';
 
 export const Badge = ({ color, onClick, className }) => (
-  <i onClick={onClick} className={`badge badge--${color} ${className}`}></i>
+  <i
+    onClick={onClick}
+    className={classNames('badge', { [`badge--${color}`]: color }, className)}
+  ></i>
 );
