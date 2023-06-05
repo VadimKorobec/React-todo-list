7f9221c920e7666b06705c43ca32c2341f1519c0
VadimKorobec <_s_e_t__@ukr.net>
2023-06-05 19:15:15 +0300

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,26 +3,45 @@ import classNames from 'classnames';
 import closeX from '../../assets/img/closeX.svg';
 import '../List/List.scss';
 import { Badge } from 'components/Badge/Badge';
+import axios from 'axios';
 
-export const List = ({ items, isRemovable, onClick, onRemove }) => {
+export const List = ({
+  items,
+  isRemovable,
+  onClick,
+  onRemove,
+  onClickItem,
+  activeItem,
+}) => {
   const removeList = item => {
     if (window.confirm('Do you want to delete the list?')) {
-      onRemove(item);
+      axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+        onRemove(item.id);
+      });
     }
   };
+
   return (
     <div>
       <ul onClick={onClick} className="list">
         {items.map((item, index) => (
           <li
             key={index}
-            className={classNames(item.className, { active: item.active })}
+            className={classNames(item.className, {
+              active: item.active
+                ? item.active
+                : activeItem && activeItem.id === item.id,
+            })}
+            onClick={onClickItem ? () => onClickItem(item) : null}
           >
-            <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
-            <span>{item.name}</span>
+            <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
+            <span>
+              {item.name}
+              {item.tasks && ` (${item.tasks.length})`}
+            </span>
             {isRemovable && (
               <img
-                onClick={removeList}
+                onClick={() => removeList(item)}
                 className="list__remove-icon"
                 src={closeX}
                 alt="close X"
